dc3695f8a65e19aab351f8506da1f2761518db09
Indra Pratama Putra <indra.symfonia@gmail.com>
2017-07-20 17:08:01 +0700

    update

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import './index.css';
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-<<<<<<< HEAD
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
@@ -20,11 +19,6 @@ function theApp() {
     </MuiThemeProvider>
   )
 }
-=======
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
-
->>>>>>> origin/master
 
 ReactDOM.render(theApp(), document.getElementById('root'));
 registerServiceWorker();
