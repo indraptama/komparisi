55dfe9ce50aa5823d1efdb450ae69701396f6426
Indra Pratama Putra <indra.symfonia@gmail.com>
2017-07-28 07:47:31 +0700

    update

--- src/Routes/Home/index.js
+++ src/Routes/Home/index.js
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
+import './style.css'
 
 export default class Home extends Component {
 
   render() {
-    return (<div>Ini Home</div>);
+    return (
+      <div className="Home">
+      Ini Home
+    </div>);
   }
 }
